a5f7ea091865d85dfd668c93607c9fbf401ae435
clbokea <clbo@kea.dk>
2017-04-07 08:29:17 +0200

    exercise

---
+++ Exercises/ExerciseWhileLoop.java
@@ -0,0 +1,16 @@
+public class ExeciseWhileLoop 
+{
+   public static void main(String[] args) 
+   {
+      // Write a for loop  
+      // that adds all numbers from 11 to 20
+      // and prints out the result.
+
+      
+      
+      // Write a while loop  
+      // that adds all numbers from 11 to 20
+      // and prints out the result.
+
+   }
+}
