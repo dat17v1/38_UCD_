26766bb140c1c3f9d5d8e71182ffba7202164d14
clbokea <clbo@kea.dk>
2017-04-07 10:46:51 +0200

    Showroom added

---
+++ Code/ShowRoom.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+
+public class ShowRoom
+{
+    public static void main(String[] args)
+    {
+            Scanner scan = new Scanner(System.in);
+
+            // String name = scan.nextLine();
+            // int number = scan.nextInt();
+            // double numberD = scan.nextDouble();
+            // System.out.println("Hello " + name + " " + number  + " " + numberD);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // String text = ""; 
+
+            // for(int i = 0; i > 0; )
+            // {
+            //     text = scan.nextLine(); 
+            //     System.out.println(text);
+               
+            //     if(text.equals("STOP"))
+            //     {
+            //         i++;   
+            //     }
+            // }
+
+
+
+            While Loop
+
+            String text = ""; 
+
+            while(!text.equals("STOP"))
+            {
+                text = scan.nextLine(); 
+                System.out.println(text);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        // System.out.print("Name: ");
+        // String name = System.console().readLine();
+
+        // System.out.println("Hello " + name);
+
+       // double value = Double.parseDouble(name);
+
+    
+      //  int result = Integer.parseInt(name);
+
+       
+    }
+
+}
